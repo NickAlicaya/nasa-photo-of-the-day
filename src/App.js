1af8755791a41dd1d5409f4e9ed37756c64a6c1c
nicanor-alicaya <donalicaya@gmail.com>
2019-11-06 16:31:25 -0500

    initial commit, axios added

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [nasaData, setNasaData] = useState([]);
+  useEffect(()=>{
+  axios.get('https://api.nasa.gov/planetary/apod?api_key=2qD76VUzXC8HEKzqNzrdKGTx0AIuSrrUEYREqewj')
+    .then((response)=>{
+    console.log(response.data);
+    })
+    .catch((error) =>{
+    console.log(error);
+  })  
+  },[])
+
   return (
+  
     <div className="App">
       <p>
         Read through the instructions in the README.md file to build your NASA
         app! Have fun 🚀!
       </p>
-    </div>
+      </div>
   );
 }
 
