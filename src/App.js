12a42e89b4b79fab7855ffc37843110d9945752f
nicanor-alicaya <donalicaya@gmail.com>
2019-11-07 18:43:10 -0500

    added navbar, card components through reactstrap

--- src/App.js
+++ src/App.js
@@ -2,26 +2,34 @@ import React, {useState, useEffect} from "react";
 import "./App.css";
 import axios from "axios";
 import NasaCard from "./component/NasaCard"
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Example from './component/Example'
 
 function App() {
   const [data, setdata] = useState([]);
+  
   useEffect(()=>{
   axios.get('https://api.nasa.gov/planetary/apod?api_key=2qD76VUzXC8HEKzqNzrdKGTx0AIuSrrUEYREqewj')
     .then((response)=>{
-    console.log(response.data);
+    console.log(response);
     setdata(response.data)
     })
     .catch((error) =>{
     console.log(error);
   })  
   },[])
-console.log(data)
   return (
+  <>
   
+  <Example/>
     <div className="App">
-    <NasaCard title={data['title']}imgUrl={data['url']} photoCaption={data['explanation']}/>
+      
+   
+    
+    <NasaCard title={data['title']} date={data['date']} imgUrl={data['url']} copyright={data['copyright']} photoCaption={data['explanation']}/>
      
     </div>
+    </>
   )
 }
 
--- src/component/NasaCard.js
+++ src/component/NasaCard.js
@@ -1,11 +1,43 @@
 import React from "react";
+import {
+    Container, Card, CardImg, CardText, CardBody,
+    CardTitle, CardSubtitle, Button,Col
+  } from 'reactstrap';
+import styled from "styled-components"
+
+const nasaTitle = styled.h1`
+font-size: 2.5rem;
+font-weight:650;
+color: slateblue;
+`
 
 const NasaCard = (props) =>{
     return(
     <div>
+    <Container>
+    <Col>
+    <Card>
+        <CardImg top width="85%" src={props.imgUrl} alt="Picture of heavenly bodies for nov 7,2019" />
+        <CardBody>
+          <CardTitle>{props.title}</CardTitle>
+          <CardSubtitle>Date: {props.date}</CardSubtitle>
+          <CardSubtitle>Copyright {props.copyright}</CardSubtitle>
+          <CardText>Description: {props.photoCaption}</CardText>
+          <Button>Subscribe</Button>
+        </CardBody>
+      </Card>
+
+
+{/* 
+        <h2>Astronomy picture of the day</h2>
         <h1>{props.title}</h1>
+        <p>Date: {props.date}</p>
         <img src= {props.imgUrl}/>
-        <p>{props.photoCaption}</p>
+        <h3>Copyright {props.copyright}</h3>
+        <p>Description: {props.photoCaption}</p> */}
+    </Col>
+    </Container>
     </div>
     )}
 export default NasaCard
+
